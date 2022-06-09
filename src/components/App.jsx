374dfecb839d3d2ce213958dd6204e2699536c31
StanislavProtashchuk <stasprotaskhyk@gmail.com>
2022-06-09 13:37:49 +0300

    pages

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,15 @@
+import {Routes, Route } from "react-router-dom";
+import AppBar from "./AppBar";
+import Home from "./Home/Home";
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <AppBar />
+      <Routes>
+        <Route path='/' element={<Home />}/>          
+      </Routes>
+      
     </div>
   );
 };
